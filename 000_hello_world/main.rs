b695563d1fcee0774f11c98dc1ac4e2001fedeeb
SamProkopchuk <ssprokopchuk@gmail.com>
2020-08-22 00:16:32 -0700

    Create le "Hello, world!" program

---
+++ 000_hello_world/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    println!("Hello, world!");
+    // println! calls a Rust macro.
+    // println would call a Rust function.
+    // The best explanation of macros in Rust I could find can be found here:
+    // https://medium.com/@phoomparin/a-beginners-guide-to-rust-macros-5c75594498f1
+}
